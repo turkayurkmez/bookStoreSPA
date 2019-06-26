11a628dbafb2b0d5e8058da76091ef19a223eea8
turkayurkmez <turkay@turkayurkmez.com>
2019-06-26 16:58:05 +0300

    adding services and component

--- BookStoreClient/src/app/app.module.ts
+++ BookStoreClient/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BookComponent } from './book/book.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BookService } from './book/book.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
